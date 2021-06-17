6d911bbe3acf4715b6ce1abf8a7411bfbdb46af0
Christoph Grimm <grimm@cs.uni-kl.de>
2021-06-17 12:46:18 +0200

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0+")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
 }
